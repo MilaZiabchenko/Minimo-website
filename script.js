1f4114bb11e4d9ea8320b3b7564e3a6983037f14
Mila Ziabchenko <mi.podgurska@gmail.com>
2020-10-02 13:14:43 +0200

    modified: index.html modified: script.js

--- script.js
+++ script.js
@@ -1,81 +1,91 @@
 const arrEmails = [];
 
-const submit = document.querySelector('#submit');
-submit.addEventListener('click', function() {
-	const validation = validateForm();
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", validateForm);
 
-	function validateForm() {
-		const email = document.forms["myForm"]["email"].value;
+function validateForm() {
+  const email = document.forms["myForm"]["email"].value;
 
-		if (email === '') {
-			setTimeout(function() {
-				const flashMessage = document.createElement('div');
-				flashMessage.innerHTML = `
-					<div class="flash red">
-						<p class="emailValidation red">email is required!</p>
-					</div>
-				`
-				document.querySelector('.flashMessageContainer').insertAdjacentElement('beforeend', flashMessage);
-			}, 500);
-			
-			setTimeout(function() {
-				document.querySelector('.flash').parentNode.removeChild(document.querySelector('.flash'));
-			}, 5000);
-
-			return false;
-		}
-		if (email.length < 8) {
-			setTimeout(function() {
-				document.querySelector('input').value = '';
-				const flashMessage = document.createElement('div');
-				flashMessage.innerHTML = `
+  if (email === "") {
+    setTimeout(function () {
+      const flashMessage = document.createElement("div");
+      flashMessage.innerHTML = `
 				<div class="flash red">
-					<p class="emailValidation red">email must be at least 8 chars!</p>
+					<p class="emailValidation red">email is required!</p>
 				</div>
-				`
-				document.querySelector('.flashMessageContainer').insertAdjacentElement('beforeend', flashMessage);
-			}, 500);
+			`;
+      document
+        .querySelector(".flashMessageContainer")
+        .insertAdjacentElement("beforeend", flashMessage);
+    }, 500);
 
-			setTimeout(function() {
-				document.querySelector('.flash').parentNode.removeChild(document.querySelector('.flash'));
-			}, 5000);
+    setTimeout(function () {
+      document
+        .querySelector(".flash")
+        .parentNode.removeChild(document.querySelector(".flash"));
+    }, 5000);
 
-			return false;
+    return false;
+  }
 
-		} else {
-			arrEmails.push(document.querySelector('input').value);
+  if (email.length < 8) {
+    setTimeout(function () {
+      document.querySelector("input").value = "";
+      const flashMessage = document.createElement("div");
+      flashMessage.innerHTML = `
+			<div class="flash red">
+				<p class="emailValidation red">email must be at least 8 chars!</p>
+			</div>
+			`;
+      document
+        .querySelector(".flashMessageContainer")
+        .insertAdjacentElement("beforeend", flashMessage);
+    }, 500);
 
-			const objEmails = {
-				data: arrEmails
-			};
-			JSON.stringify(objEmails);
-			const strEmails = JSON.stringify(objEmails);
-			JSON.parse(strEmails);
+    setTimeout(function () {
+      document
+        .querySelector(".flash")
+        .parentNode.removeChild(document.querySelector(".flash"));
+    }, 5000);
 
-			localStorage.setItem('emails', strEmails);
+    return false;
+  } else {
+    arrEmails.push(document.querySelector("input").value);
 
-			setTimeout(function() {
-				document.querySelector('input').value = '';
-				const flashMessage = document.createElement('div');
-				flashMessage.innerHTML = `
+    const objEmails = {
+      data: arrEmails,
+    };
+    JSON.stringify(objEmails);
+    const strEmails = JSON.stringify(objEmails);
+    JSON.parse(strEmails);
+
+    localStorage.setItem("emails", strEmails);
+
+    setTimeout(function () {
+      document.querySelector("input").value = "";
+      const flashMessage = document.createElement("div");
+      flashMessage.innerHTML = `
 				<div class="flash green">
 					<p class="emailValidation green">your email is accepted :)</p>
 				</div>
-				`
-				document.querySelector('.flashMessageContainer').insertAdjacentElement('beforeend', flashMessage);
-			}, 500);
-			
-			setTimeout(function() {
-				document.querySelector('.flash').parentNode.removeChild(document.querySelector('.flash'));
-			}, 5000);
-		}
-	}
-});
+				`;
+      document
+        .querySelector(".flashMessageContainer")
+        .insertAdjacentElement("beforeend", flashMessage);
+    }, 500);
 
-const button = document.querySelector('.loadMore');
-button.addEventListener('click', function() {
-	let loadItems = document.createElement('div');
-	loadItems.innerHTML = `
+    setTimeout(function () {
+      document
+        .querySelector(".flash")
+        .parentNode.removeChild(document.querySelector(".flash"));
+    }, 5000);
+  }
+}
+
+const button = document.querySelector(".loadMore");
+button.addEventListener("click", function () {
+  let loadItems = document.createElement("div");
+  loadItems.innerHTML = `
 	<div class="wrapper">
 		<div class="row row4">
 			<article class="smallPost leftPost">
@@ -96,12 +106,12 @@ button.addEventListener('click', function() {
 			</article>
 		</div>
 	</div>
-	`
-	document.querySelector('.row3').insertAdjacentElement('afterend', loadItems);
+	`;
+  document.querySelector(".row3").insertAdjacentElement("afterend", loadItems);
 
-	setTimeout(function() {
-	loadItems = document.createElement('div');
-	loadItems.innerHTML = `
+  setTimeout(function () {
+    loadItems = document.createElement("div");
+    loadItems.innerHTML = `
 	<div class="wrapper">
 		<div class="row row5">
 			<article class="smallPost leftPost">
@@ -122,13 +132,15 @@ button.addEventListener('click', function() {
 			</article>
 		</div>
 	</div>
-	`
-	document.querySelector('.row4').insertAdjacentElement('afterend', loadItems);
-	}, 1500);
+	`;
+    document
+      .querySelector(".row4")
+      .insertAdjacentElement("afterend", loadItems);
+  }, 1500);
 
-	setTimeout(function() {
-	loadItems = document.createElement('div');
-	loadItems.innerHTML = `
+  setTimeout(function () {
+    loadItems = document.createElement("div");
+    loadItems.innerHTML = `
 	<div class="wrapper">
 		<div class="row row6">
 			<article class="smallPost leftPost">
@@ -149,7 +161,9 @@ button.addEventListener('click', function() {
 			</article>
 		</div>
 	</div>
-	`
-	document.querySelector('.row5').insertAdjacentElement('afterend', loadItems);
-	}, 3000);
-})
+	`;
+    document
+      .querySelector(".row5")
+      .insertAdjacentElement("afterend", loadItems);
+  }, 3000);
+});
