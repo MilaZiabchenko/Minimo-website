e46d8f4d907729cc91527241a77b4803355b778a
Mila Ziabchenko <mi.podgurska@gmail.com>
2021-10-27 22:33:35 +0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const overlay = document.querySelector('.overlay');
 const links = document.querySelectorAll('.overlay a');
 
 menuBtn.addEventListener('click', () => {
-	menuBtn.classList.toggle('open');
-	overlay.classList.toggle('open');
+  menuBtn.classList.toggle('open');
+  overlay.classList.toggle('open');
 });
 
 links.forEach(link => {
-	link.addEventListener('click', () => {
-		menuBtn.classList.toggle('open');
-		overlay.classList.toggle('open');
-	});
+  link.addEventListener('click', () => {
+    menuBtn.classList.toggle('open');
+    overlay.classList.toggle('open');
+  });
 });
 
 // Email validation
@@ -20,76 +20,76 @@ const submit = document.querySelector('#submit');
 submit.addEventListener('click', validateEmail);
 
 function validateEmail(e) {
-	e.preventDefault();
+  e.preventDefault();
 
-	const arrEmails = [];
-	const email = document.forms['contact']['email'].value;
-	document.querySelector('form > input').value = '';
+  const arrEmails = [];
+  const email = document.forms['contact']['email'].value;
+  document.querySelector('form > input').value = '';
 
-	// Creating flash message
-	function createFlashMessage(p, className) {
-		setTimeout(() => {
-			const flashMessage = document.createElement('div');
-			flashMessage.innerHTML = p;
-			flashMessage.classList.add('flash', className);
-			document
-				.querySelector('.flash-message-container')
-				.insertAdjacentElement('beforeend', flashMessage);
-		}, 500);
-	}
+  // Creating flash message
+  function createFlashMessage(p, className) {
+    setTimeout(() => {
+      const flashMessage = document.createElement('div');
+      flashMessage.innerHTML = p;
+      flashMessage.classList.add('flash', className);
+      document
+        .querySelector('.flash-message-container')
+        .insertAdjacentElement('beforeend', flashMessage);
+    }, 500);
+  }
 
-	// Removing flash message
-	function removeFlashMessage() {
-		setTimeout(() => {
-			const flash = document.querySelector('.flash');
-			flash.remove();
-		}, 5000);
-	}
+  // Removing flash message
+  function removeFlashMessage() {
+    setTimeout(() => {
+      const flash = document.querySelector('.flash');
+      flash.remove();
+    }, 5000);
+  }
 
-	if (email === null || email === '') {
-		createFlashMessage(`<p>email address is required!</p>`, 'invalid');
-		removeFlashMessage();
+  if (email === null || email === '') {
+    createFlashMessage(`<p>email address is required!</p>`, 'invalid');
+    removeFlashMessage();
 
-		return false;
-	} else if (email.includes('@') === false) {
-		createFlashMessage(`<p>email must include '@'!</p>`, 'invalid');
-		removeFlashMessage();
+    return false;
+  } else if (email.includes('@') === false) {
+    createFlashMessage(`<p>email must include '@'!</p>`, 'invalid');
+    removeFlashMessage();
 
-		return false;
-	} else if (email.length < 8) {
-		createFlashMessage(`<p>email must be at least 8 chars!</p>`, 'invalid');
-		removeFlashMessage();
+    return false;
+  } else if (email.length < 8) {
+    createFlashMessage(`<p>email must be at least 8 chars!</p>`, 'invalid');
+    removeFlashMessage();
 
-		return false;
-	} else {
-		createFlashMessage(`<p>your email is accepted :)</p>`, 'valid');
-		removeFlashMessage();
+    return false;
+  } else {
+    createFlashMessage(`<p>your email is accepted :)</p>`, 'valid');
+    removeFlashMessage();
 
-		arrEmails.push(email);
+    arrEmails.push(email);
 
-		const objEmails = {
-			data: arrEmails
-		};
+    const objEmails = {
+      data: arrEmails,
+    };
 
-		// Stringifying valid email values and adding them to local storage
-		localStorage.setItem('emails', JSON.stringify(objEmails));
+    // Stringifying valid email values and adding them to local storage
+    localStorage.setItem('emails', JSON.stringify(objEmails));
 
-		return arrEmails;
-	}
+    return arrEmails;
+  }
 }
 
 // Creating class for adding some more blogs
 class Blog {
-	constructor(imageTitle, imageAltText, topic, heading) {
-		this.imageTitle = imageTitle;
-		this.imageAltText = imageAltText;
-		this.topic = topic;
-		this.heading = heading;
-		this.text = paragraph;
-	}
+  constructor(imageTitle, imageAltText, topic, heading) {
+    this.imageTitle = imageTitle;
+    this.imageAltText = imageAltText;
+    this.topic = topic;
+    this.heading = heading;
+    this.text = paragraph;
+  }
 
-	createArticle() {
-		const article = `
+  createArticle() {
+    const article = `
 		<article class="small-post">
 			<img class="image" src="/img/${this.imageTitle}.jpg" alt="${this.imageAltText}">
 			<h4><a href="#">${this.topic}</a></h4>
@@ -98,68 +98,68 @@ class Blog {
 		</article>
 		`;
 
-		return article;
-	}
+    return article;
+  }
 }
 
 const paragraph = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...`;
 
 // Instantiating an array of new blog objects
 const blogs = [
-	new Blog(
-		'adorable kitten',
-		'An adorable kitten',
-		'photodiary',
-		'Boundless tenderness'
-	),
-	new Blog('cup of coffee', 'A cup of coffee', 'lifestyle', 'Taste of morning'),
-	new Blog(
-		'glass chess',
-		'Glass chess pieces on the chess board',
-		'lifestyle',
-		'Mind games'
-	),
-	new Blog(
-		'stylish sunglasses',
-		'Stylish sunglasses',
-		'lifestyle',
-		'Watery shades of black'
-	),
-	new Blog(
-		'old woods',
-		'Big trees in the forest',
-		'travel',
-		'My friend the forest'
-	),
-	new Blog('old boots', 'Stylish boots', 'photodiary', 'Half the world behind')
+  new Blog(
+    'adorable kitten',
+    'An adorable kitten',
+    'photodiary',
+    'Boundless tenderness'
+  ),
+  new Blog('cup of coffee', 'A cup of coffee', 'lifestyle', 'Taste of morning'),
+  new Blog(
+    'glass chess',
+    'Glass chess pieces on the chess board',
+    'lifestyle',
+    'Mind games'
+  ),
+  new Blog(
+    'stylish sunglasses',
+    'Stylish sunglasses',
+    'lifestyle',
+    'Watery shades of black'
+  ),
+  new Blog(
+    'old woods',
+    'Big trees in the forest',
+    'travel',
+    'My friend the forest'
+  ),
+  new Blog('old boots', 'Stylish boots', 'photodiary', 'Half the world behind'),
 ];
 
 // Loading newly created blogs
 const btnLoadMore = document.querySelector('#loadmore');
 btnLoadMore.addEventListener('click', () => {
-	function addRow(prevBlog, nextBlog, nextRow, prevRow) {
-		const loadItems = document.createElement('section');
-		loadItems.innerHTML = `
+  function addRow(prevBlog, nextBlog, nextRow, prevRow) {
+    const loadItems = document.createElement('section');
+    loadItems.innerHTML = `
 			<div class="grid row ${nextRow}">
 				${prevBlog.createArticle()}
 				${nextBlog.createArticle()}
 			</div>
 		`;
-		loadItems.classList.add('container');
-		document
-			.querySelector(prevRow)
-			.insertAdjacentElement('afterend', loadItems);
+    loadItems.classList.add('container');
+    document
+      .querySelector(prevRow)
+      .insertAdjacentElement('afterend', loadItems);
 
-		return loadItems;
-	}
+    return loadItems;
+  }
 
-	addRow(blogs[0], blogs[1], 'row4', '.row3');
+  addRow(blogs[0], blogs[1], 'row4', '.row3');
 
-	setTimeout(() => {
-		addRow(blogs[2], blogs[3], 'row5', '.row4');
-	}, 1500);
+  setTimeout(() => {
+    addRow(blogs[2], blogs[3], 'row5', '.row4');
+  }, 1500);
 
-	setTimeout(() => {
-		addRow(blogs[4], blogs[5], 'row6', '.row5');
-	}, 3000);
+  setTimeout(() => {
+    addRow(blogs[4], blogs[5], 'row6', '.row5');
+  }, 3000);
 });
