d11b6eddfd787f211414689f72820cd602b84864
Mila Ziabchenko <mi.podgurska@gmail.com>
2022-04-14 14:47:27 +0300

    modified:   index.html 	new file:   js/constants.js 	renamed:    script.js -> js/script.js

---
+++ js/constants.js
@@ -0,0 +1,25 @@
+export const blogText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...`;
+
+export const blogData = [
+  [
+    'adorable kitten',
+    'An adorable kitten',
+    'photodiary',
+    'Boundless tenderness',
+  ],
+  ['cup of coffee', 'A cup of coffee', 'lifestyle', 'Taste of morning'],
+  [
+    'glass chess',
+    'Glass chess pieces on the chess board',
+    'lifestyle',
+    'Mind games',
+  ],
+  [
+    'stylish sunglasses',
+    'Stylish sunglasses',
+    'lifestyle',
+    'Watery shades of black',
+  ],
+  ['old woods', 'Big trees in the forest', 'travel', 'My friend the forest'],
+  ['old boots', 'Stylish boots', 'photodiary', 'Half the world behind'],
+];
--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+import { blogText, blogData } from './constants.js';
+
 const menuBtn = document.querySelector('.menu-btn');
 const overlay = document.querySelector('.overlay');
 const links = document.querySelectorAll('.overlay a');
@@ -105,46 +107,7 @@ class BlogContent {
   }
 }
 
-const blogText = `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...`;
-
-const newBlogs = [
-  new BlogContent(
-    'adorable kitten',
-    'An adorable kitten',
-    'photodiary',
-    'Boundless tenderness'
-  ),
-  new BlogContent(
-    'cup of coffee',
-    'A cup of coffee',
-    'lifestyle',
-    'Taste of morning'
-  ),
-  new BlogContent(
-    'glass chess',
-    'Glass chess pieces on the chess board',
-    'lifestyle',
-    'Mind games'
-  ),
-  new BlogContent(
-    'stylish sunglasses',
-    'Stylish sunglasses',
-    'lifestyle',
-    'Watery shades of black'
-  ),
-  new BlogContent(
-    'old woods',
-    'Big trees in the forest',
-    'travel',
-    'My friend the forest'
-  ),
-  new BlogContent(
-    'old boots',
-    'Stylish boots',
-    'photodiary',
-    'Half the world behind'
-  ),
-];
+const newBlogs = blogData.map(item => new BlogContent(...item));
 
 const addRowWithNewBlogs = (prevBlog, nextBlog, nextRow, prevRow) => {
   const loadItems = document.createElement('section');
